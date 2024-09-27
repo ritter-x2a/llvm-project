e5120910abbf59580ed7f4bbd0ef9d7c1ecee571
Fabian Ritter <fabian.ritter@amd.com>
2024-09-27 03:25:04 -0400

    [AMDGPU] Enable unaligned scratch accesses
    This allows us to emit wide generic and scratch memory accesses when we do not
    have alignment information. In cases where accesses happen to be properly
    aligned or where generic accesses do not go to scratch memory, this improves
    performance of the generated code by a factor of up to 16x and reduces code
    size, especially when lowering memcpy and memmove intrinsics.
    
    Also: Make the use of the FeatureUnalignedScratchAccess feature more
    consistent: Code has already assumed that unaligned accesses with the
    specialized flat scratch instructions are allowed independent of
    FeatureUnalignedScratchAccess at some places. This patch always uses this
    interpretation.
    
    Part of SWDEV-455845.

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -590,6 +590,10 @@ public:
     return UnalignedScratchAccess;
   }
 
+  bool hasUnalignedScratchAccessEnabled() const {
+    return UnalignedScratchAccess && UnalignedAccessMode;
+  }
+
   bool hasUnalignedAccessMode() const {
     return UnalignedAccessMode;
   }
